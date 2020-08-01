39c0da710e19677b8f32ad25c86df59235c6d61e
calin <Commodork64>
2020-08-01 14:26:37 +0100

    Cleaned up draw code, put into seperate function.

--- main.d
+++ main.d
@@ -20,93 +20,93 @@ import std.stdio, std.conv;
  *                                                                             *
  *----------------------------------------------------------------------------*/
 
-int main() {
+int main()
+{
 
-    //variables to store states of elements within the game
+    // variables to store states of elements within the game
     int playerMove;
-    int goDecider = 0; // whose go is it
-    // represent the board and intialise all values to ' '
+    int goDecider = 0; // keeps track of whose go it is.
+    // create the board, and intialise all values to ' '
     char[3][3] board = ' ';
 
     writeln("TIC TAC TOE by Calin Clement Dermott, 2020");
 
-    //instructions
+    // Instructions
     writeln("The grid goes left to right, 1-3, 3 times. So choosing the number");
     writeln("four would put an 'X' in the left most column on the second row.");
-    
-    while(true) {
-        //Get player input
+
+    while (true)
+    {
+        // Get player input
         writeln("enter a number from 1-9");
         readf(" %d", playerMove);
-        
-        
+
         // Put player input into the board
         updateBoard(board, playerMove, goDecider);
-        // Draw the board (Need to refactor this)
-        writeln();
-        write(board[0][0]);
-        write(" ");
-        write(board[0][1]);
-        write(" ");
-        write(board[0][2]);
-        writeln();
-        write(board[1][0]);
-        write(" ");
-        write(board[1][1]);
-        write(" ");
-        write(board[1][2]);
-        writeln();
-        write(board[2][0]);
-        write(" ");
-        write(board[2][1]);
-        write(" ");
-        write(board[2][2]);
-        writeln();
+        // Draw the board
+        drawBoard(board);
         goDecider++;
     }
     return 0;
 }
 
-void updateBoard(ref char[3][3] board, int coordinate, int goDecider) {
+void updateBoard(ref char[3][3] board,
+        int coordinate, int goDecider)
+{
 
-    switch( coordinate ) {
-        case 1:
-            board[0][0] = characterDecider(goDecider);
-            break;
-        case 2:
-            board[0][1] = characterDecider(goDecider);
-            break;
-        case 3:
-            board[0][2] = characterDecider(goDecider);
-            break;
-        case 4:
-            board[1][0] = characterDecider(goDecider);
-            break;
-        case 5:
-            board[1][1] = characterDecider(goDecider);
-            break;
-        case 6:
-            board[1][2] = characterDecider(goDecider);
-            break;
-        case 7:
-            board[2][0] = characterDecider(goDecider);
-            break;
-        case 8:
-            board[2][1] = characterDecider(goDecider);
-            break;
-        case 9:
-            board[2][2] = characterDecider(goDecider);
-            break;
-        default:
-            break;
+    switch (coordinate)
+    {
+    case 1:
+        board[0][0] = characterDecider(goDecider);
+        break;
+    case 2:
+        board[0][1] = characterDecider(goDecider);
+        break;
+    case 3:
+        board[0][2] = characterDecider(goDecider);
+        break;
+    case 4:
+        board[1][0] = characterDecider(goDecider);
+        break;
+    case 5:
+        board[1][1] = characterDecider(goDecider);
+        break;
+    case 6:
+        board[1][2] = characterDecider(goDecider);
+        break;
+    case 7:
+        board[2][0] = characterDecider(goDecider);
+        break;
+    case 8:
+        board[2][1] = characterDecider(goDecider);
+        break;
+    case 9:
+        board[2][2] = characterDecider(goDecider);
+        break;
+    default:
+        break;
     }
 }
 
-char characterDecider(int goDecider) {
-    if (goDecider % 2 == 0) {
+char characterDecider(int goDecider)
+{
+    if (goDecider % 2 == 0)
+    {
         return 'X';
     }
-    else {
+    else
+    {
         return 'O';
+    }
+}
+
+void drawBoard(char[3][3] board)
+{
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            write(board[i][j]);
+            write(" ");
+        }
+        writeln();
     }
 }
