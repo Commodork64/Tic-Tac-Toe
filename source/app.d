e2a3a881cfc9bc74c292a92ab4f42c62bd93bd96
calin <Commodork64>
2020-08-06 17:28:18 +0100

    game now sending the server the updated board.

--- source/app.d
+++ source/app.d
@@ -1,4 +1,4 @@
-import std.stdio, std.socket;
+import std.stdio, std.socket, std.conv;
 import utility;
 
 /*******************************************************************************
@@ -29,15 +29,47 @@ int main()
     int goDecider = 0; // keeps track of whose go it is.
     // create the board, and intialise all values to ' '
     char[3][3] board = ' ';
+    bool winnerDecided = false;
     writeln("TIC TAC TOE by Calin Clement Dermott, 2020");
 
     // networking setup
     auto socket = new Socket(AddressFamily.INET, SocketType.DGRAM);
     socket.bind(new InternetAddress(InternetAddress.ADDR_ANY,
                     InternetAddress.PORT_ANY));
+
+    // Instructions
+    writeln("The grid goes left to right, 1-3, 3 times. So choosing the number");
+    writeln("four would put an 'X' in the left most column on the second row.");
+
+    while (true)
+    {
+        // Get player input
+        writeln("enter a number from 1-9");
+        readf(" %d", playerMove);
+
+        // Put player input into the board
+        updateBoard(board, playerMove, goDecider);
+        updateServer(board, socket);
+
+        // Receive updated board from server.
+
+        // Draw the board
+        drawBoard(board);
+        goDecider++;
     
-    // This chunk of code will need to be altered later, only using for testing.
-    string dataPacket = "hello";
+        if(winnerDecided) { break; }
+    }
+
+    return 0;
+}
+
+// I will need to split this out into a seperate file
+void updateServer(char[3][3]board, ref Socket socket) {
+    // sends the current layout of the board to the server.
+
+    // converts two dimensional array into string to be sent.
+    auto dataPacket = to!string(board);
+
     writeln("Sending Data...");
     auto err = socket.sendTo(dataPacket, new InternetAddress("localhost", 9000));
     if (err == Socket.ERROR) { throw new Exception(lastSocketError()); }
@@ -52,25 +84,4 @@ int main()
 	auto data = buffer[0 .. err];
 	writeln("Got response: ", data);
 
-    return 0;
-
-    // Instructions
-    writeln("The grid goes left to right, 1-3, 3 times. So choosing the number");
-    writeln("four would put an 'X' in the left most column on the second row.");
-
-    while (true)
-    {
-        // Get player input
-        writeln("enter a number from 1-9");
-        readf(" %d", playerMove);
-
-        // Put player input into the board
-        updateBoard(board, playerMove, goDecider);
-        // Draw the board
-        drawBoard(board);
-        goDecider++;
-    }
-    // Will this be reachable or will the game loop back to waiting to connect
-	// to the server?
-	return 0;
 }
